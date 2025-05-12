29c285d3955f0a6e6ac10cbccfaddc43dd4a587d
Rohan R <126628269+RohanR21@users.noreply.github.com>
2025-05-12 15:17:13 -0400

    Autoplay and centered

--- app/components/ActiveSlide.tsx
+++ app/components/ActiveSlide.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { UCProjectBox } from "../components/UnclickableCard";
@@ -15,14 +15,20 @@ function ActiveSlide() {
     //Margin to the sides
     <div className="mx-6 mt-8">
       <Swiper
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 5000, //Changes every 5 seconds
+          disableOnInteraction: true, //Autoplay stops after user interaction
+        }}
+        centeredSlides={true} //Makes the middle slide the center instead of the most left (can be changed)
         spaceBetween={20}
         slidesPerView={3}
-        //I Genuinly didn't know loop was an attribute and spent 30 minutes tying to hardcode this
-        loop={true}
+        loop={true} //I Genuinly didn't know loop was an attribute and spent 30 minutes tying to hardcode this
         //These next 2 lines can be removed
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
         //For Mobile and smaller screens
+        //Tested points based on your text overflow or whatever
         breakpoints={{
           320: {
             slidesPerView: 1,
